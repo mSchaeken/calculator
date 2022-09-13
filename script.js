bee69158764e6fa025146520d61908c3ffbf1fe5
MathijsSchaeken <mathijs.schaeken@outlook.com>
2022-09-13 08:53:46 +0200

    Create addListeners function
    Created a function which attaches eventlisteners to all the calculator
    buttons based on their HTML class. Added call to function on windowload.

--- script.js
+++ script.js
@@ -20,4 +20,31 @@ function divide() {
 
 };
 
+function clearNumber () {
 
+};
+
+function enterNumber () {
+
+};
+
+
+function addListeners() {
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach(button => {
+        
+        switch (button.className) {
+            case 'operand':
+                button.addEventListener('click', enterNumber);
+                break;
+            case 'operator':
+                button.addEventListener('click', operate);
+                break;
+            case 'clear':
+                button.addEventListener('click', clearNumber);
+                break;
+        };
+    });
+};
+
+window.onload = addListeners();
